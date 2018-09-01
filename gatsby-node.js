dbb91671ef7ef94903bcf1b22d43d6f74eb7bdc6
Burak Tokak <iletisim@buraktokak.com>
2018-09-01 11:17:21 +0300

    add default settings, add options into docs

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,16 +1,20 @@
 const CompressionPlugin = require("compression-webpack-plugin");
 
 exports.onCreateWebpackConfig = ({ stage, actions }, options) => {
+  if(!options){
+    // Default options for ordinary gatsby project.
+    options = {
+      asset: '[path].gz[query]',
+      algorithm: 'gzip'
+    };
+  }
   switch (stage) {
     case `build-html`:
     case `build-javascript`:
     case `build-css`:
       actions.setWebpackConfig({
         plugins: [
-          new CompressionPlugin({
-            asset: '[path].gz[query]',
-            algorithm: 'gzip'
-          })
+          new CompressionPlugin(options)
         ],
       })
   }
