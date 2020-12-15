f00fc03a89b668782df1711bf4c651a9c9ce12ac
Samuel Suther <info@suther.de>
2020-12-15 14:55:39 +0100

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,19 +1,21 @@
 const CompressionPlugin = require("compression-webpack-plugin");
 
-exports.onCreateWebpackConfig = ({ getConfig, stage }, options) => {
-  const config = getConfig();
+exports.onCreateWebpackConfig = ({ stage, actions }, options) => {
+  if(!options){
+    // Default options for ordinary gatsby project.
+    options = {
+      asset: '[path].gz[query]',
+      algorithm: 'gzip'
+    };
+  }
   switch (stage) {
     case `build-html`:
     case `build-javascript`:
-    case `build-css`: {
-      config.plugin(`compression`,
-        CompressionPlugin,
-        [{asset: '[path].gz[query]', algorithm: 'gzip'}]
-      );
-      return config
-    }
-    default: {
-      return config
-    }
+    case `build-css`:
+      actions.setWebpackConfig({
+        plugins: [
+          new CompressionPlugin(options)
+        ],
+      })
   }
 }
